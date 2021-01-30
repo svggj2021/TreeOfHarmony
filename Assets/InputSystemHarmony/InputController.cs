4af9764175f5f16ca40344d5b14cf66f94af93ca
gouravscivista <gourav@scivista.com>
2021-01-30 16:08:48 -0700

    packages removed

---
+++ Assets/InputSystemHarmony/InputController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public GameObject cube;
+    float timecounter = 0;
+    Dictionary<float, List<KeyPressedWeight>> stored;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+     /*   if(Input.GetButtonDown("Horizontal"))
+        float translation = Input.GetAxis("Vertical");
+        float rotation = Input.GetAxis("Horizontal");
+        float jump = Input.GetAxis("Jump");*/
+
+        if(PlayerSceneMode.mode==PlayerSceneMode.PlayerSceneModeType.Fighting)
+        {
+            
+        }
+        
+    }
+}
---
+++ Assets/InputSystemHarmony/PlayerSceneMode.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSceneMode : MonoBehaviour
+{
+   public enum PlayerSceneModeType
+    {
+        Exploring,Fighting
+    }
+
+    public static PlayerSceneModeType mode=PlayerSceneModeType.Exploring;
+}
