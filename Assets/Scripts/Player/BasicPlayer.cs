7bebcd1f7cf5b002c4ea1247d61b74f7c797c1e0
Daniel <sirspaceanchor@gmail.com>
2021-01-30 16:42:58 -0700

    re-organized scripts and placeholder player folders

---
+++ Assets/Scripts/Player/BasicPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicPlayer : MonoBehaviour
+{
+    // Struct containing player_stats
+    public struct PlayerStats
+    {
+        public float movement_speed;
+        private Vector2 move_dir;
+    }
+
+
+    // Reference RigidBody
+    public Rigidbody2D physicsTarget;    
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // Get Input Data
+    void get_input()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Inpt.GetAxis("Vertical");
+    }
+
+    void get_movement(Vector2 direction)
+    {
+
+    }
+}
