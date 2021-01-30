286c08377c1967d3db19a337b60afbb7c09f5cb2
Daniel <sirspaceanchor@gmail.com>
2021-01-30 14:20:26 -0700

    pushed prototype timer
    just a simple timer

---
+++ Assets/Scripts/BeatTimer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class BeatTimer : MonoBehaviour
+{
+    
+    // Measure is unused at this time
+    [Serializable]
+    public struct TimerVariables
+    {
+        public float measure;
+        public float bpm;
+        public float minutesInSeconds;
+    }
+
+    //Expose Time Variables 
+    public TimerVariables TimeSettings;
+    
+    // Private variable for current time
+    private float beat_time;
+
+    void Update()
+    {
+        update_beat();
+    }
+
+    // Checks for a beat ever X seconds
+    public void update_beat()
+    {
+        if(Time.time > beat_time)
+        {
+            beat_time = Time.time + TimeSettings.bpm / TimeSettings.minutesInSeconds;
+            
+            // Call functions here to be executed every X seconds
+            
+            // This is just a debug
+            Debug.Log(Mathf.Round(beat_time) + " Seconds has elapsed");
+        }
+    }
+}
