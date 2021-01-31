a74c0d92c3b600a45a9b8a144f549cabcf2d4db7
Daniel <sirspaceanchor@gmail.com>
2021-01-30 19:06:51 -0700

    Added some placeholders, finished player script

--- Assets/Scripts/Player/BasicPlayer.cs
+++ Assets/Scripts/Player/BasicPlayer.cs
@@ -9,8 +9,14 @@ public class BasicPlayer : MonoBehaviour
     [Serializable]
     public struct PlayerStats
     {
+        [Header("Movement Settings")]
         public float moveSpeed;
-        public float jumpForce;
+
+        [Header("Jump Settings")]
+        [Range(1,10)]
+        public float verticalForce;
+        public float fallModifier;
+        public float jumpModifier;
     }
 
     // Reference RigidBody
@@ -18,45 +24,71 @@ public class BasicPlayer : MonoBehaviour
 
     // Expose player stats
     public PlayerStats stats;
-
+    
+    // Movement direction vector
     private Vector2 move_dir;
 
-    void Start()
-    {
-        
-    }
+    // Jump boolean
+    private bool can_jump;
 
     void Update()
     {
         get_input();
-        apply_movement(move_dir);
     }
 
-    // Get Input Data
+    void FixedUpdate()
+    {
+        apply_movement(move_dir);
+        apply_gravity_modifier();
+    }
+
+    // Get Input Data, use for any input checks
     void get_input()
     {
         // Horizontal Movement
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
-<<<<<<< Updated upstream
-=======
         move_dir = new Vector2(x,y);
 
         // Vertical Movement
-        if Input
->>>>>>> Stashed changes
+        if(Input.GetButtonDown("Jump"))
+        {
+            can_jump = true;
+        }
     }
 
     // Apply Movement
     void apply_movement(Vector2 direction)
     {
         physicsTarget.velocity = (new Vector2(move_dir.x * stats.moveSpeed, physicsTarget.velocity.y));
+
+        if(can_jump)
+        {
+            physicsTarget.velocity = new Vector2(physicsTarget.velocity.x, 0);
+            physicsTarget.AddForce(Vector2.up * stats.verticalForce, ForceMode2D.Impulse);
+            can_jump = false;
+        }
     }
 
-    // Apply Jump
-    void apply_jump()
+    // Apply gravity mods
+    void apply_gravity_modifier()
     {
-        physicsTarget.velocity = new Vector2(physicsTarget.velocity.x, 0);
-        physicsTarget.velocity += Vector2.up * stats.jumpForce;
+        // Apply Gravity
+        if (physicsTarget.velocity.y < 0)
+        {
+            physicsTarget.gravityScale = stats.fallModifier;
+        }
+
+        // Apply Jump based on input being pressed
+        else if(physicsTarget.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            physicsTarget.gravityScale = stats.jumpModifier;
+        }   
+        
+        // If nothing else applies, reset gravity to it's default
+        else
+        {
+            physicsTarget.gravityScale = 1.0f;
+        }
     }
 }
